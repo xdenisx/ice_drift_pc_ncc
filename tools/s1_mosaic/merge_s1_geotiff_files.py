dedc0cb7cc9d46cee61b9b66c234616864bb8153
xdenisx <demchev@gmail.com>
2021-06-08 13:36:55 +0000

    mosaic extanded to 2 days

--- tools/s1_mosaic/merge_s1_geotiff_files.py
+++ tools/s1_mosaic/merge_s1_geotiff_files.py
@@ -15,6 +15,137 @@ from datetime import datetime, timedelta
 from skimage import exposure
 import re
 
+def apply_anisd(img, gamma=0.25, step=(1., 1.), kappa=50, ploton=False):
+    """
+    Anisotropic diffusion.
+
+    Usage:
+    imgout = anisodiff(im, niter, kappa, gamma, option)
+
+    Arguments:
+            img    - input image
+            niter  - number of iterations
+            kappa  - conduction coefficient 20-100 ?
+            gamma  - max value of .25 for stability
+            step   - tuple, the distance between adjacent pixels in (y,x)
+            option - 1 Perona Malik diffusion equation No 1
+                     2 Perona Malik diffusion equation No 2
+            ploton - if True, the image will be plotted on every iteration
+
+    Returns:
+            imgout   - diffused image.
+
+    kappa controls conduction as a function of gradient.  If kappa is low
+    small intensity gradients are able to block conduction and hence diffusion
+    across step edges.  A large value reduces the influence of intensity
+    gradients on conduction.
+
+    gamma controls speed of diffusion (you usually want it at a maximum of
+    0.25)
+
+    step is used to scale the gradients in case the spacing between adjacent
+    pixels differs in the x and y axes
+
+    Diffusion equation 1 favours high contrast edges over low contrast ones.
+    Diffusion equation 2 favours wide regions over smaller ones.
+
+    Reference:
+    P. Perona and J. Malik.
+    Scale-space and edge detection using ansotropic diffusion.
+    IEEE Transactions on Pattern Analysis and Machine Intelligence,
+    12(7):629-639, July 1990.
+
+    Original MATLAB code by Peter Kovesi
+    School of Computer Science & Software Engineering
+    The University of Western Australia
+    pk @ csse uwa edu au
+    <http://www.csse.uwa.edu.au>
+
+    Translated to Python and optimised by Alistair Muldal
+
+    Sep 2017 modified by Denis Demchev
+    """
+
+    # init args
+
+    # Conduction coefficient
+    kappa = kappa
+
+    # Number of iterations
+    niter = 10
+
+    # Number of equation (1,2)
+    option = 1
+
+    if img.ndim == 3:
+        warnings.warn("Only grayscale images allowed, converting to 2D matrix")
+        img = img.mean(2)
+
+    # initialize output array
+    img = img.astype('float32')
+    imgout = img.copy()
+
+    # initialize some internal variables
+    deltaS = np.zeros_like(imgout)
+    deltaE = deltaS.copy()
+    NS = deltaS.copy()
+    EW = deltaS.copy()
+    gS = np.ones_like(imgout)
+    gE = gS.copy()
+
+    # create the plot figure, if requested
+    if ploton:
+        import pylab as pl
+
+        fig = pl.figure(figsize=(20, 5.5), num="Anisotropic diffusion")
+        ax1, ax2 = fig.add_subplot(1, 2, 1), fig.add_subplot(1, 2, 2)
+
+        ax1.imshow(img, interpolation='nearest')
+        ih = ax2.imshow(imgout, interpolation='nearest', animated=True)
+        ax1.set_title("Original image")
+        ax2.set_title("Iteration 0")
+
+        fig.canvas.draw()
+
+    for ii in range(niter):
+
+        # calculate the diffs
+        deltaS[:-1, :] = np.diff(imgout, axis=0)
+        deltaE[:, :-1] = np.diff(imgout, axis=1)
+
+        # conduction gradients (only need to compute one per dim!)
+        if option == 1:
+            gS = np.exp(-(deltaS / kappa) ** 2.) / step[0]
+            gE = np.exp(-(deltaE / kappa) ** 2.) / step[1]
+        elif option == 2:
+            gS = 1. / (1. + (deltaS / kappa) ** 2.) / step[0]
+            gE = 1. / (1. + (deltaE / kappa) ** 2.) / step[1]
+
+        # update matrices
+        E = gE * deltaE
+        S = gS * deltaS
+
+        # subtract a copy that has been shifted 'North/West' by one
+        # pixel. don't as questions. just do it. trust me.
+        NS[:] = S
+        EW[:] = E
+        NS[1:, :] -= S[:-1, :]
+        EW[:, 1:] -= E[:, :-1]
+
+        # update the image
+        imgout += gamma * (NS + EW)
+
+        if ploton:
+            iterstring = "Iteration %i" % (ii + 1)
+            ih.set_data(imgout)
+            ax2.set_title(iterstring)
+            fig.canvas.draw()
+            # sleep(0.01)
+
+    p2, p98 = np.nanpercentile(imgout, (2, 98))
+    im = exposure.rescale_intensity(imgout, in_range=(p2, p98), out_range=(0, 254))
+    return im
+
 def gtiff_enhance(tif_path, out_path):
     # Open mosaic
     ds_mos = gdal.Open(tif_path)
@@ -29,7 +160,7 @@ def gtiff_enhance(tif_path, out_path):
     # Contrast enhance
     ###################################
     # Map values to the (0, 255) range:
-
+    '''
     A0 = (A0 - np.nanmin(A0)) * 254.0 / (np.nanmax(A0) - np.nanmin(A0))
 
     # Kernel for negative Laplacian:
@@ -47,6 +178,7 @@ def gtiff_enhance(tif_path, out_path):
     A = A0 + Laps
     # Set negative values to 0, values over 254 to 254:
     A = np.clip(A, 0, 254)
+    '''
     ###################################
     # END contrast enhance
     ###################################
@@ -55,15 +187,28 @@ def gtiff_enhance(tif_path, out_path):
     #A = median(A, disk(3))
 
     # Contrast stretching
-    p2, p98 = np.nanpercentile(A, (2, 98))
-    A = exposure.rescale_intensity(A, in_range=(p2, p98), out_range=(0, 254))
+    #from skimage.filters.rank import mean_bilatera
+    from skimage.restoration import denoise_tv_chambolle
+    print('\nStart filtering...\n')
+    #A = denoise_tv_chambolle(A0, weight=10)#median(A0, disk(1))
+    #A = apply_anisd(A0)
+    #№from skimage.restoration import denoise_bilateral
+    #A = denoise_bilateral(A0)
+    #print('\nEnd filtering\n')
+
+    p2, p98 = np.nanpercentile(A0, (2, 98))
+    A = exposure.rescale_intensity(A0, in_range=(p2, p98), out_range=(0, 254))
 
     # Gamma correction
-    #A = exposure.adjust_gamma(A, 0.7)
+    A = exposure.adjust_gamma(A, 1.6)
+
+    print('\nMedian filtering...')
+    A = median(A, disk(1))
+    print('\nDone.')
 
     # Rescale again
-    #p2, p98 = np.nanpercentile(A, (2, 98))
-    #A = exposure.rescale_intensity(A, in_range=(p2, p98), out_range=(0, 254))
+    p2, p98 = np.nanpercentile(A, (2, 98))
+    A = exposure.rescale_intensity(A, in_range=(p2, p98), out_range=(0, 254))
 
     A[np.isnan(A)] = 255
 
@@ -92,7 +237,7 @@ except:
     pass
 
 td = datetime.today()
-days = 1
+days = 2
 
 dt_2 = datetime.today()
 dt_1 = datetime.today() - timedelta(days=days)
@@ -113,7 +258,7 @@ for root, dirs, files in os.walk(in_path):
 
 print(files_to_mosaic)
 
-files_to_mosaic.sort(key=lambda x: os.path.basename(x).split('_')[6])
+files_to_mosaic.sort(key=lambda x: os.path.basename(x).split('_')[6], reverse=True)
 
 #files_string = (" ".join(files_to_mosaic))
 #print(files_string)
@@ -124,7 +269,7 @@ out_title = '%s_%s' % (os.path.basename(files_to_mosaic[0]).split('_')[6],
                        os.path.basename(files_to_mosaic[-1]).split('_')[6])
 '''
 
-out_title = '%s' % os.path.basename(files_to_mosaic[-1]).split('_')[6][0:8]
+out_title = '%s' % os.path.basename(files_to_mosaic[0]).split('_')[6][0:8]
 
 # Path to 'full' tiff mosaic
 tif_path = '%s/S1_mos_%s.tif' % (out_path, out_title)
@@ -136,7 +281,14 @@ out_path = '%s/ps_S1_mos_%s.tif' % (out_path, out_title)
 
 print('\nStart making mosaic from %s files...' % len(files_to_mosaic))
 # "COMPRESS=LZW"
+try:
+    os.remove(tif_path)
+except:
+    pass
+
 g = gdal.Warp(tif_path, files_to_mosaic, format="GTiff", options=["BIGTIFF=YES", "TILED=YES"],
+              xRes=300., yRes=300.,
+              dstSRS='EPSG:3995',
               srcNodata=np.nan,
               dstNodata=-999.)
 print('Done.\n')
