1e3ef5d5e09b65228fa5803341e69b6f2f59ea22
andyGFHill <tiburonhildemon@gmail.com>
2021-10-13 19:12:32 +0200

    Save polarizations as band in same image
    Now, 'geotiff_collocation' will save images from different polarizations
    into the same pair of collocated geotiffs. The different polarizations
    are instead saved as different bands. Which polarization that is part of
    which band can be seen by the description of each band in the metadata
    of the geotiff files.

--- tools/geotiff_collocation/collocate.py
+++ tools/geotiff_collocation/collocate.py
@@ -89,64 +89,109 @@ def check_save_pair(f1, f2, out_path, id_pair, polarizations, intersect_ratio, m
     
     try:
 
-
-        dir_made = False
+        adjuster1 = RasterAdjuster(f1, f1)
+        adjuster2 = RasterAdjuster(f2, f2)
+        bands1 = []
+        bandNames = []
+        bands2 = []
 
         # Loop through all polarizations
-        for polarization in polarizations:
-            # print("Going through polarization: %s" % polarization)
-            # See if current polarization exists
-            f1_polarization = re.sub(r"UPS_\w{2}_", "UPS_" + polarization + "_", f1  )
-            f2_polarization = re.sub(r"UPS_\w{2}_", "UPS_" + polarization + "_", f2  )
-            if not (os.path.isfile(f1_polarization) and os.path.isfile(f2_polarization)):
-                continue
+        try:
 
-            raster1_export_path='%s/%03d/%s' % (out_path, id_pair, os.path.basename(f1_polarization))
-            raster2_export_path='%s/%03d/%s' % (out_path, id_pair, os.path.basename(f2_polarization))
-            mask_export_path='%s/%03d/mask_%s' % (out_path, id_pair, os.path.basename(f1_polarization))
-            if ( os.path.isfile( raster1_export_path ) ):
-                continue
+            for polarization in polarizations:
+                # print("Going through polarization: %s" % polarization)
+                # See if current polarization exists
+                f1_polarization = re.sub(r"UPS_\w{2}_", "UPS_" + polarization + "_", f1  )
+                f2_polarization = re.sub(r"UPS_\w{2}_", "UPS_" + polarization + "_", f2  )
+                if not (os.path.isfile(f1_polarization) and os.path.isfile(f2_polarization)):
+                    continue
+                bandNames.append( polarization )
+                # Acquire cropped band from current polarization to list of bands for file 1
+                tempRaster = gdal.Open( f1_polarization )
+                adjuster1.initFromRasters( adjuster1.raster1, tempRaster )
+                # Acquire cropped band from current polarization to list of bands for file 2
+                tempRaster = gdal.Open( f2_polarization )
+                adjuster2.initFromRasters( adjuster2.raster1, tempRaster )
 
-            print('\nStart adjusment...')
+                # Acquire cropped band from current polarization to list of bands for file 2
+                bands1.append( adjuster1.raster2.GetRasterBand(1).ReadAsArray() )
+                bands2.append( adjuster2.raster2.GetRasterBand(1).ReadAsArray() )
+        except Exception as e:
+            print("Failed to crop all bands to same raster!")
+            raise e
+            return 0
+        if len( bands1 ) == 0:
+            return 0
+        print("Bands: %s" % str(bandNames))
 
-            adjuster = RasterAdjuster(f1_polarization, f2_polarization, intersection_extension = extension)
-            # Get arrays of rasters
-            array1 = adjuster.raster1.ReadAsArray()
-            array2 = adjuster.raster2.ReadAsArray()
-            # Acquire the area of non-empty pixels in the rasters
-            area1 = ~( np.isnan(array1) | (array1 == 0) )
-            area2 = ~( np.isnan(array2) | (array2 == 0) )
-            intersection_area = area1 & area2
-            # Acquire the number of pixels for intersection
-            intersection_area = np.sum( intersection_area )
-            area1 = np.sum( area1 )
-            area2 = np.sum( area2 )
-            # If some of the images do not have any information
-            if ( area1 == 0 ):
-                print( "\nNo active pixels after cut away on file: %s" % os.path.basename(f1_polarization) )
-                continue
-            if ( area2 == 0 ):
-                print( "\nNo active pixels after cut away on file: %s" % os.path.basename(f2_polarization) )
-                continue
-            # If intersection area is too small
-            if ( intersection_area < intersect_ratio * np.min( (area1, area2) ) ):
-                print( "\nToo small of an overlap for images: \n%s and \n%s" % ( os.path.basename(f1_polarization), os.path.basename(f2_polarization) ) )
-                continue
-                
-            print('\n### Start making pair... ###')
-            # Create dir for a pir
-            if not dir_made:
-                try:
-                    os.makedirs('%s/%03d' % (out_path, id_pair))
-                    dir_made = True
-                except:
-                    print("Failed to make directory: %s/%03d" % (out_path, id_pair) )
-                    return 0
-                
-            adjuster.export(raster1_export_path=raster1_export_path,
-                            raster2_export_path=raster2_export_path,
-                            mask_export_path=mask_export_path)
-            print('Adjusment done.\n')
+        try:
+            # Populate all bands
+            tempRaster = gdal.GetDriverByName('GTiff').Create( "%s/temp1" % out_path , adjuster1.raster1.RasterXSize, adjuster1.raster1.RasterYSize, len(bands1), gdal.GDT_Float32 )
+            tempRaster.SetGCPs( adjuster1.raster1.GetGCPs(), adjuster1.raster1.GetGCPProjection() )
+            tempRaster.SetGeoTransform( adjuster1.raster1.GetGeoTransform() )
+            tempRaster.SetProjection( adjuster1.raster1.GetProjectionRef() )
+            for iter in range(len(bands1)):
+                tempRaster.GetRasterBand(int(iter+1)).WriteArray(bands1[iter])
+                tempRaster.GetRasterBand(int(iter+1)).SetDescription(bandNames[iter])
+            tempRaster.SetMetadata( dict( zip(["Band " + str(iter) for iter in range(len(bandNames))], bandNames ) ) )
+            adjuster1.initFromRasters( tempRaster, tempRaster )
+
+            tempRaster2 = gdal.GetDriverByName('GTiff').Create( "%s/temp2" % out_path , adjuster2.raster1.RasterXSize, adjuster2.raster1.RasterYSize, len(bands2), gdal.GDT_Float32 )
+            tempRaster2.SetGCPs( adjuster2.raster1.GetGCPs(), adjuster2.raster1.GetGCPProjection() )
+            tempRaster2.SetGeoTransform( adjuster2.raster1.GetGeoTransform() )
+            tempRaster2.SetProjection( adjuster2.raster1.GetProjectionRef() )
+            for iter in range(len(bands2)):
+                tempRaster2.GetRasterBand(int(iter+1)).WriteArray(bands2[iter])
+                tempRaster2.GetRasterBand(int(iter+1)).SetDescription(bandNames[iter])
+            tempRaster.SetMetadata( dict( zip(["Band " + str(iter) for iter in range(len(bandNames))], bandNames ) ) )
+            adjuster2.initFromRasters( tempRaster2, tempRaster2 )
+        except Exception as e:
+            print("Failed to add all bands to rasters!")
+            raise e
+            return 0
+
+
+        print('\nStart adjusment...')
+
+        adjuster1.initFromRasters( adjuster1.raster1, adjuster2.raster1, intersection_extension = extension)
+        # Get arrays of rasters
+        array1 = adjuster1.get_raster1_as_array()
+        array2 = adjuster1.get_raster2_as_array()
+        # Acquire the area of non-empty pixels in the rasters
+        area1 = ~( np.isnan(array1) | (array1 == 0) )
+        area2 = ~( np.isnan(array2) | (array2 == 0) )
+        intersection_area = area1 & area2
+        # Acquire the number of pixels for intersection
+        intersection_area = np.sum( intersection_area )
+        area1 = np.sum( area1 )
+        area2 = np.sum( area2 )
+        # If some of the images do not have any information
+        if ( area1 == 0 ):
+            print( "\nNo active pixels after cut away on file: %s" % os.path.basename(f1_polarization) )
+            return 0
+        if ( area2 == 0 ):
+            print( "\nNo active pixels after cut away on file: %s" % os.path.basename(f2_polarization) )
+            return 0
+        # If intersection area is too small
+        if ( intersection_area < intersect_ratio * np.min( (area1, area2) ) ):
+            print( "\nToo small of an overlap for images: \n%s and \n%s" % ( os.path.basename(f1_polarization), os.path.basename(f2_polarization) ) )
+            return 0
+            
+        print('\n### Start making pair... ###')
+        # Create dir for a pir
+        try:
+            os.makedirs('%s/%03d' % (out_path, id_pair))
+        except:
+            print("Failed to make directory: %s/%03d" % (out_path, id_pair) )
+            return 0
+            
+        raster1_export_path='%s/%03d/%s' % (out_path, id_pair, os.path.basename(f1))
+        raster2_export_path='%s/%03d/%s' % (out_path, id_pair, os.path.basename(f2))
+        mask_export_path='%s/%03d/mask_%s' % (out_path, id_pair, os.path.basename(f1))
+        adjuster1.export(raster1_export_path=raster1_export_path,
+                        raster2_export_path=raster2_export_path,
+                        mask_export_path=mask_export_path)
+        print('Adjusment done.\n')
 
         return 1
 
@@ -359,16 +404,14 @@ if __name__ == "__main__":
 
                                 # Get ID 
                                 id_pair = acquireID(out_path) 
-                                print('\nTime lag is %.1f [hours]' % abs((dt_i - dt0).total_seconds() / 3600))
-                                print('\nMaking pair %03d ... ' % id_pair)
-                                print('\nFile 1: %s \nFile 2: %s' % (ifile, ifile2) )
+                                # print('\nFile 1: %s \nFile 2: %s' % (ifile, ifile2) )
                                 # Save pair 
                                 res = check_save_pair(ifile, ifile2, out_path, id_pair, polarizations, intersect_ratio, maximum_drift_speed = max_drift_speed)
     
                                 if res == 1:
+                                    print('\nTime lag is %.1f [hours]' % abs((dt_i - dt0).total_seconds() / 3600))
+                                    print('\nMaking pair %03d ... ' % id_pair)
                                     print('Done.\n')
-                                else:
-                                    print('Failed.\n')
                     else:
                         pass
 
