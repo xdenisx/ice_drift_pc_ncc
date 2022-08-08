785a5364ad8379c340f528bb86c7648c8e7c420c
Denis Demchev <denis@quartz.rss.chalmers.se>
2022-08-08 12:35:18 +0200

    Raster export in fast ice code

--- tools/fast_ice/fast_ice.py
+++ tools/fast_ice/fast_ice.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from skimage import measure
-from osgeo import gdal
+from osgeo import gdal, ogr
 import numpy as np
 import os
 import sys
@@ -9,13 +9,12 @@ sys.path.append("/data/rrs/seaice/esa_rosel/code/ice_drift_pc_ncc/tools/geolocat
 from LocationMapping import LocationMapping
 import geojson
 
-
 class get_fast_ice:
 	'''
 	Class for fast ice deliniation from ice drift data
 	'''
 
-	def __init__(self, npz_file_path=None, tiff_file_path=None, stp=50, th_pixels=5):
+	def __init__(self, npz_file_path=None, tiff_file_path=None, stp=50, th_pixels=3):
 		self.npz_file_path = npz_file_path
 		self.tiff_file_path = tiff_file_path
 		self.stp = stp
@@ -32,8 +31,13 @@ class get_fast_ice:
 		Y = np.arange(self.image.RasterYSize)
 		X, Y = np.meshgrid(X, Y)
 		lat, lon = lm.raster2LatLon(X.reshape((-1)), Y.reshape((-1)))
-		self.lats = lat.reshape(self.image.ReadAsArray()[0].shape[0], self.image.ReadAsArray()[0].shape[1])
-		self.lons = lon.reshape(self.image.ReadAsArray()[0].shape[0], self.image.ReadAsArray()[0].shape[1])
+
+		if self.image.RasterCount > 1:
+			self.lats = lat.reshape(self.image.ReadAsArray()[0].shape[0], self.image.ReadAsArray()[0].shape[1])
+			self.lons = lon.reshape(self.image.ReadAsArray()[0].shape[0], self.image.ReadAsArray()[0].shape[1])
+		else:
+			self.lats = lat.reshape(self.image.ReadAsArray().shape[0], self.image.ReadAsArray().shape[1])
+			self.lons = lon.reshape(self.image.ReadAsArray().shape[0], self.image.ReadAsArray().shape[1])
 
 		# Separate stable (fast) ice (1) and drift ice (0) into two classes based on the threshold
 		self.ice_drift_magnitude[self.ice_drift_magnitude <= self.th_pixels] = 1
@@ -63,8 +67,12 @@ class get_fast_ice:
 		Export result to Geojson file
 		'''
 
-		f_dir = os.path.dirname(out_fname)
-		os.makedirs(f_dir, exist_ok=True)
+		feature_collection = {"type": "FeatureCollection",
+							  "features": []
+							  }
+
+		f_name = out_fname  # os.path.dirname(out_fname)
+		os.makedirs(os.path.dirname(out_fname), exist_ok=True)
 
 		for ch, contour in enumerate(self.contours):
 			b_lons = []
@@ -76,15 +84,26 @@ class get_fast_ice:
 				b_lats.append(self.lats[int(contour[i][0] * self.stp + self.stp),
 										int(contour[i][1] * self.stp + self.stp)])
 
-			# Define Polygon string
 			poly_str = [[x for x in zip(*[iter(b_lons), iter(b_lats)])]]
 			features = geojson.Polygon(poly_str)
+			feature_collection["features"].append(geojson.Feature(geometry=features, properties={}))
 
-			# feature is a shapely geometry type
-			geom_in_geojson = geojson.Feature(geometry=features, properties={})
+		with open(out_fname, 'w') as outfile:
+			geojson.dump(feature_collection, outfile)
 
-			out_fname = f'{f_dir}/{ch}_fast_ice.geojson'
-			with open(out_fname, 'w') as outfile:
-				geojson.dump(geom_in_geojson, outfile)
+	def export_raster(self, fn_vec, fn_ras, out_fname):
+		'''
+		Export to geotiff
+		'''
 
-
+		ras_ds = gdal.Open(fn_ras)
+		vec_ds = ogr.Open(fn_vec)
+		lyr = vec_ds.GetLayer()
+		geot = ras_ds.GetGeoTransform()
+		drv_tiff = gdal.GetDriverByName("GTiff")
+		chn_ras_ds = drv_tiff.Create(out_fname, ras_ds.RasterXSize, ras_ds.RasterYSize, 1, gdal.GDT_Float32)
+		chn_ras_ds.SetGeoTransform(geot)
+		chn_ras_ds.SetProjection(ras_ds.GetProjection())
+		gdal.RasterizeLayer(chn_ras_ds, [1], lyr)
+		chn_ras_ds.GetRasterBand(1).SetNoDataValue(0.0)
+		chn_ras_ds = None
